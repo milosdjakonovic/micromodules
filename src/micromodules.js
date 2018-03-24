e5e8bfece3953d0614944afd999cf0c2025b032c
Miloš Đakonović <milos@djakonovic.net>
2018-03-24 20:37:36 +0100

    Delayed module functionallity

--- src/micromodules.js
+++ src/micromodules.js
@@ -1,5 +1,17 @@
 (function(w){
 
+    //simple array iterator
+    function _a(array, method){
+        var templ=array.length;
+        for(var i=0;i<templ;i++){
+          method.call(this, i, array[i] );
+        }
+    }
+
+
+
+
+
     var
 
     _module = {},
@@ -21,6 +33,20 @@
         
     },
 
+    declareQueue = [
+        // Example:
+        // {args: declare function arguments, unmet_module:'name of unmet module'}
+    ],
+
+    processUnmet = function(id){
+        _a(declareQueue, function(index,elem){
+            if(elem.unmet_module === id){
+                declare.apply(w, elem.args)
+                delete(declareQueue[index]);                
+            }
+        });
+    },
+
     /**
      * Declares module
      * module HAS to be named
@@ -31,16 +57,33 @@
 
         if( (typeof(id) === "string") && (typeof(cbOrDepsArr) === "function") ){ // clean situation, id and cb
             _module[id]=cbOrDepsArr.apply(w);
+            processUnmet(id);
             return _module[id];//
         }
         else if( w.Object.prototype.toString.call( cbOrDepsArr ) === '[object Array]' ){
             //here we have deps
-            var forCbArray = [];
-            for(var i=0; i<cbOrDepsArr.length;i++ ){
-                forCbArray.push( _module[cbOrDepsArr[i]] );
+            var forCbArray = [], unmet=false;
+            for(var i=0; i<cbOrDepsArr.length; i++ ){
+                var modName = cbOrDepsArr[i];
+                if(typeof( _module[modName] ) !== "undefined"){
+                    forCbArray.push( _module[modName] );
+                } else {
+                    unmet = true;
+                    declareQueue.push({
+                        args: arguments,
+                        unmet_module: modName
+                    });
+                }
             }
-            _module[id]=possibleCb.apply(w, forCbArray);
-            return _module[id];
+            if(!unmet){
+                _module[id]=possibleCb.apply(w, forCbArray);
+                _a(forCbArray, function(ind,el){
+                    processUnmet(el);
+                })
+                return _module[id];
+            } else{
+                return false;
+            }
         }
         else if( 
                 (typeof(id)          === "string")   &&
@@ -50,6 +93,7 @@
                    //or   declare('somelongtextmodule', 525454545)
                    //or   declare('somelongtextmodule', {jsondata...})
             _module[id] = cbOrDepsArr;
+            processUnmet(id);
             return _module[id];//
         } else {
             throw new Error('Incorrect arguments provided for declare function. Aborting.');
@@ -58,7 +102,9 @@
 
     };
 
-    declare._modules = _module;//For debug purposes, may be stripped out
+    //For debug purposes, may be stripped out:
+    declare._modules = _module;
+    declare._unmet   = declareQueue
 
     w.provide = provide;
     w.declare = declare;
