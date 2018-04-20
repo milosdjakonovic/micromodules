2f57bb0067c80f7b166fb0501374c66f08ed2b44
Miloš Đakonović <milos@djakonovic.net>
2018-04-20 23:04:41 +0200

    Rewrite

--- src/micromodules.js
+++ src/micromodules.js
@@ -1,119 +1,83 @@
-(function(w){
-
-    //simple array iterator
-    function _a(array, method){
-        //var templ=array.length;
-        for(var i=0;i<array.length;i++){
-          method.call(this, i, array[i] );
-        }
-    }
-
-
-
-
+(function(win){
     var
-
-    _module = {},
+    /**
+     * modules internal object
+     */
+    $modules = {},
 
     /**
-     * Pulls module:
-     *  provide(['de1', 'de2'], function(de1, de2){
-     *      
-     *  });
-     * 
+     * issue warning when needed
+     * @param txt - warning text
      */
-    provide = function(deps, cb){
-
-        var forCbArray = [], dontRun=false;
-        for(var i=0; i<deps.length;i++ ){
-            if (typeof( _module[deps[i]] ) !== "undefined")
-                forCbArray.push( _module[deps[i]] );
-            else dontRun = true;
-        }
-        if(dontRun) return false;
-        return cb.apply(w, forCbArray);
-        
-    },
-
-    declareQueue = [
-        // Example:
-        // {args: declare function arguments, unmet_module:'name of unmet module'}
-    ],
-
-    processUnmet = function(id){
-
-        var LdeclareQueue = declareQueue;declareQueue=[];
-
-        for(i=0;i<LdeclareQueue.length;i++){
-            if(LdeclareQueue[i].unmet_module === id){
-                var taj = LdeclareQueue.splice(i,1);
-                declare.apply(w, taj[0].args);
-            }
-        }
-        declareQueue = declareQueue.concat(LdeclareQueue);
-
+    warn = function(txt){
+        (console ? (console.warn || console.log) : function (m) { return m; })(txt);
     },
 
     /**
-     * Declares module
-     * module HAS to be named
-     * declare('id', cb);
-     * declare('id', ["dep1", "dep2"], cb);
+     * return the value of the module
+     * @param name - the name of the module
      */
-    declare = function(id, cbOrDepsArr, possibleCb){
-        //Case: declare('id', cb);
-        if( (typeof(id) === "string") && (typeof(cbOrDepsArr) === "function") ){
-            _module[id]=cbOrDepsArr.apply(w);
-            processUnmet(id);
-            return _module[id];//
-        }
-        else if( w.Object.prototype.toString.call( cbOrDepsArr ) === '[object Array]' ){
-            //case: declare('id', ['dep1', 'dep2'], cb)
-            var forCbArray = [], unmet=false;
-            for(var i=0; i<cbOrDepsArr.length; i++ ){
-                var modName = cbOrDepsArr[i];
-                if(typeof( _module[modName] ) !== "undefined"){
-                    forCbArray.push( _module[modName] );
-                } else {
-                    unmet = true;// (modName === 'five') && log('ovde, za:' , id, ' - ', declare.caller)
-                    declareQueue.push({
-                        args: arguments,
-                        unmet_module: modName
-                    });
-                }
-            }
-            if(!unmet){
-                _module[id]=possibleCb.apply(w, forCbArray);
-                _a(forCbArray, function(ind,el){
-                    processUnmet(el);
-                })
-                return _module[id];
-            } else{
-                return false;
-            }
-        }
-        else if( 
-                (typeof(id)          === "string")   &&
-                (typeof(cbOrDepsArr) !== "function") &&
-                (w.Object.prototype.toString.call( cbOrDepsArr ) !== '[object Array]')
-                ){ //case declare('somelongtextmodule', "LOrem ipsum..................")
-                   //or   declare('somelongtextmodule', 525454545)
-                   //or   declare('somelongtextmodule', {jsondata...})
-            _module[id] = cbOrDepsArr;
-            processUnmet(id);
-            return _module[id];//
+    fninclude = function(){
+        var name = arguments[0];
+        //if( typeof($modules[ name ])!== 'undefined')
+        if( name in $modules ){ 
+            if(typeof $modules[name] === "object" && "_bindeD_fn_$" in $modules[name] &&   ! ("fnval" in $modules[name])    ){
+                $modules[name].fnval = $modules[name].fn.apply(win, [fninclude]);
+                return $modules[name].fnval;
+            } else if(
+                typeof $modules[name] === "object" &&
+                 "_bindeD_fn_$" in $modules[name] &&
+                  "fnval" in $modules[name] ){
+                return $modules[name]['fnval']
+            } else
+                return $modules[ name ]
+        } else if(
+           // <script data-md="somename" type="text/n"> </script>           
+            win.document.querySelector( 'script[type="text/n"][data-md=' + name + ']' )
+        ){
+            var thatScr = win.document.querySelector( 'script[type="text/n"][data-md=' + name + ']' ),
+            ret = Function(thatScr.innerHTML)(win);
+            $modules[name] = ret;
+            thatScr.parentNode.removeChild(thatScr);
+            return ret;
+        } else if ( name in win ){
+            $modules[name] = win[name];
+            return win[name]
         } else {
-            throw new Error('Incorrect arguments provided for declare function. Aborting.');
+            warn("Failed: module [" + name + "] does not exist.");
             return false;
         }
+    },
 
+    fndeclare = function(){
+        var name = arguments[0], value = arguments[1];
+        if(typeof($modules[name])==="undefined"){
+            if(typeof value === "function"){
+                //$modules[name] = value.apply(window, [fninclude]);
+                $modules[name] = {
+                    _bindeD_fn_$ : true,
+                    fn          : value//,
+                    //fnval: undefined
+                }
+            }
+            else 
+                $modules[name] = value;
+            return true;
+        } else {
+            //mod already defined
+            warn("Declaring failed: module [" + name + "] already declared.");
+            return false;
+        }
     };
 
-    //For debug purposes, may be stripped out:
-    declare._modules = _module;
-    declare._unmet   = declareQueue
+    fndeclare.remove = function(key){
+        if(typeof($modules[key])==="undefined")
+            return false;
+        delete $modules[key];
+        return true;
+    }
 
-    w.provide = provide;
-    w.declare = declare;
-
+    win.declare = fndeclare;
+    win.include = fninclude;
 })(window);
+
