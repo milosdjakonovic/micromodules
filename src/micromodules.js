3d5e93981d0aa9e2d15101fab587042674e8b3c0
Miloš Đakonović <milos@djakonovic.net>
2020-01-13 01:48:56 +0100

    Introduced async functionality.

--- src/micromodules.js
+++ src/micromodules.js
@@ -11,7 +11,31 @@
      * @param txt - warning text
      */
     warn = function(txt){
-        (console) ? console.log(txt) : false;
+        try{
+            console.warn(txt);
+        } catch(e){}
+    },
+
+    ComplexMod = function(fn){
+        this.fn = fn;
+    },
+
+    onDeclared = {}// { exampModule: [cb1, cb2] }
+    ,
+
+    declared = function(nm){
+        if(nm in onDeclared)
+            if(length in onDeclared[nm]){
+                for(var i=0;i<onDeclared[nm].length;i++){
+                    var singleCb = onDeclared[nm][i];
+                    var arg1;
+                    if($modules[nm] instanceof ComplexMod && !$modules[nm]['fnval'])
+                        arg1 = fninclude(nm);
+                    else arg1 = $modules[nm];
+                    singleCb.apply(win, [ arg1, fninclude]);//cleanup needed
+                    onDeclared[nm].splice(i, 1);
+                }
+            }
     },
 
     /**
@@ -20,16 +44,24 @@
      */
     fninclude = function(){
         var name = arguments[0], thatScr;
-        //if( typeof($modules[ name ])!== 'undefined')
-        if( name in $modules ){ 
-            if(typeof $modules[name] === "object" && "_bindeD_fn_$" in $modules[name] &&   ! ("fnval" in $modules[name])    ){
-                $modules[name].fnval = $modules[name].fn.apply(win, [fninclude]);
-                return $modules[name].fnval;
+        var on = name.match(/^\s*?on\s*\:\s*(.+)$/m); var cb = false;
+        if(
+            on &&
+            typeof arguments[1] === 'function'
+        ){
+            name = on[1];
+            cb   = arguments[1];
+        }
+        if( name in $modules ){
+            var toInclude = $modules[name];
+            if(typeof toInclude === "object" && toInclude instanceof ComplexMod && !("fnval" in toInclude)){
+                toInclude.fnval = toInclude.fn.apply(win, [fninclude]);
+                return toInclude.fnval;
             } else if(
-                typeof $modules[name] === "object" &&
-                 "_bindeD_fn_$" in $modules[name] &&
-                  "fnval" in $modules[name] ){
-                return $modules[name]['fnval']
+                typeof toInclude === "object" &&
+                toInclude instanceof ComplexMod &&
+                  "fnval" in toInclude ){
+                return toInclude['fnval']
             } else
                 return $modules[ name ]
         } else if(
@@ -39,11 +71,16 @@
             $modules[name] = ret;
             thatScr.parentNode.removeChild(thatScr);
             return ret;
-        } else if ( name.slice(0,-1) in win && /*deep include*/ name.match(/\+$/) ){
+        } else if ( !cb && name.slice(0,-1) in win && /*deep include*/ name.match(/\+$/) ){
             name = name.slice(0,-1);
             $modules[name] = win[name];
             return win[name]
-        } else {
+        }
+        else if(cb){
+            onDeclared[name] = onDeclared[name] || [];
+            onDeclared[name].push(cb);
+        }
+        else {
             warn("Failed: module [" + name + "] does not exist.");
             return undefined;
         }
@@ -53,15 +90,11 @@
         var name = arguments[0], value = arguments[1];
         if(typeof($modules[name])==="undefined"){
             if(typeof value === "function"){
-                //$modules[name] = value.apply(window, [fninclude]);
-                $modules[name] = {
-                    _bindeD_fn_$ : true,
-                    fn          : value//,
-                    //fnval: undefined
-                }
+                $modules[name] = new ComplexMod(value);
             }
             else 
                 $modules[name] = value;
+            declared(name);
             return true;
         } else {
             //mod already defined
